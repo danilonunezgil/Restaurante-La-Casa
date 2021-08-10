f051488707f0e38975946a4e3b1f10d5d206a89e
Diego <dialmy2094@gmail.com>
2021-08-09 22:06:01 -0500

    add template

--- main/views.py
+++ main/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>It worked!</h1>'
-                        '<div>Made by <a href="http://encisosystems.com" target="_blank">Enciso Systems</a></div>')
+    return render(request, "base.html")
+    #return HttpResponse('<h1>It worked!</h1>'
+     #                   '<div>Made by <a href="http://encisosystems.com" target="_blank">Enciso Systems</a></div>')
