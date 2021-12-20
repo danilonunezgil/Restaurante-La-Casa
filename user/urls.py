254b23a327bcfa57bb26ccccae10f8aaac6aa29b
Diego <dialmy2094@gmail.com>
2021-12-20 15:33:23 -0500

    add login and logout

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 #general urls
 app_name = 'user'
-urlpatterns = [    ]
+urlpatterns = [
+    path(route='login/', view=views.LoginView.as_view(),name='login'),
+    path(route='logout/', view=views.LogoutView.as_view(), name='logout'),
+]
 
 if not settings.DEBUG:    
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
 # Create your views here.
+
+
+class LoginView(auth_views.LoginView):
+    """Login view."""
+
+    template_name = 'login.html'
+
+
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
+
+    template_name = 'logged_out.html'
