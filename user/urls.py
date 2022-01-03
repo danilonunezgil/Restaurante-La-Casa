1a36319b7f9aa1e466c1ce46307d8b1843d7471f
Diego <dialmy2094@gmail.com>
2022-01-03 16:44:41 -0500

    User registration implementation

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.base import View
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +9,9 @@ app_name = 'user'
 urlpatterns = [
     path(route='login/', view=views.LoginView.as_view(),name='login'),
     path(route='logout/', view=views.LogoutView.as_view(), name='logout'),
+    path(route='signup/', view=views.SignupView.as_view(), name='signup' ),
+    path(route='me/profile/', view=views.UpdateProfileView.as_view(), name='update' ),
+    path(route='<str:username>/', view=views.UserDetailView.as_view(), name='detail' ),
 ]
 
 if not settings.DEBUG:    
