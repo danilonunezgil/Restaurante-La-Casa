c867b753a4137655e9ca12a1e2b78672fdbbd30d
Diego <dialmy2094@gmail.com>
2022-03-09 15:10:00 -0500

    fixed translations

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
 from .models import Product, OrderItem, Order
 # Register your models here.
 
-admin.site.register(Product)
+#admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+@admin.register(Product)
+class ProductAdmin(TranslatableAdmin):
+    pass
