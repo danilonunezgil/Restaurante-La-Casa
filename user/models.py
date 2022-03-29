78dd157b14257b4c914cee2fcc93fe7a45619e64
Cristian Renteria <derseck1102@gmail.com>
2022-03-29 14:59:36 -0500

    Update cv field in person model

--- user/models.py
+++ user/models.py
@@ -34,6 +34,7 @@ class Person(models.Model):
         help_text=_("Enter your e-mail"))
     cv = models.FileField(
         verbose_name = _('CVs'),
+        blank=True,
         upload_to='CVs'
     )
     cellphone = models.CharField(
